49635d8bcd33e289f1345877c5fd3f84adda65fe
tfongue <hadrien.fongue@prodyna.com>
2020-10-27 17:14:31 +0100

    product detail as async variable declared

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../services/products.service';
 import { Product } from '../models/product';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,6 +12,7 @@ import { Product } from '../models/product';
 export class ProductDetailComponent implements OnInit {
   id: string;
   product: Product;
+  product$: Observable<Product>;
 
   constructor(private route: ActivatedRoute, private productService: ProductsService) { }
 
