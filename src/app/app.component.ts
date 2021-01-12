6c605e31ece22dfd050a1e90b9ae32552b4aa0b0
Taghuo Fongue <fonguetaghuo@gmail.com>
2021-01-12 14:23:53 +0100

    add mat toolbar as header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LanguageService } from './service/language.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'cave-enki-frontend';
+export class AppComponent implements OnInit {
+
+  title = `Ets les caves d'Enki`;
+
+  constructor(private languageService: LanguageService) { }
+
+  ngOnInit(): void {
+    this.languageService.initialize();
+  }
+
+  selectLang(language: string): void {
+    this.languageService.setCurrentLanguage(language);
+  }
 }
